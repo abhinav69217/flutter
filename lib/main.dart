977a0fab548f2c13b3393dbff92a0066c469a986
abhinav69217 <abhinav0045.be21@chitkara.edu.in>
2024-07-17 17:04:08 +0530

    expanded, flex

--- lib/main.dart
+++ lib/main.dart
@@ -1,51 +1,54 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(home: Home()));
+void main() => runApp(const MaterialApp(home: Home()));
 
 class Home extends StatelessWidget {
+  const Home({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("MY FIRST APP"),
+        title: const Text("MY FIRST APP"),
         backgroundColor: Colors.green[700],
         centerTitle: true,
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: <Widget>[
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: <Widget>[
-              Text("Hello World"),
-              Text("Hello World"),
-            ],
+      body: Row(
+        children: [
+          Expanded(child: Image.asset("assets/space-3.jpg"), flex: 3),
+          Expanded(
+            flex: 1,
+            child: Container(
+              padding: const EdgeInsets.all(30.0),
+              color: Colors.red,
+              child: const Text("1"),
+            ),
           ),
-          Container(
-            color: Colors.cyan,
-            padding: EdgeInsets.all(20.0),
-            child: Text("Inside Container"),
+          Expanded(
+            flex: 1,
+            child: Container(
+              padding: const EdgeInsets.all(30.0),
+              color: Colors.pinkAccent,
+              child: const Text("2"),
+            ),
           ),
-          Container(
-            color: Colors.amber,
-            padding: EdgeInsets.all(30.0),
-            child: Text("Inside Container"),
+          Expanded(
+            flex: 1,
+            child: Container(
+              padding: const EdgeInsets.all(30.0),
+              color: Colors.amber,
+              child: const Text("3"),
+            ),
           ),
-          Container(
-            color: Colors.pink,
-            padding: EdgeInsets.all(40.0),
-            child: Text("Inside Container"),
-          )
         ],
       ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           print("Hello");
         },
-        child: Text("Click Me"),
         backgroundColor: Colors.green[700],
+        child: const Icon(Icons.add),
       ),
     );
   }
