b82b7ab80603288fa5f310e0e11025452bbeb86b
Abhinav <abhinav0045.be21@chitkara.edu.in>
2024-07-18 10:24:55 +0530

    custom class, rendering data via map

--- lib/main.dart
+++ lib/main.dart
@@ -1,106 +1,38 @@
 import 'package:flutter/material.dart';
+import 'quote.dart';
 
-void main() => runApp(MaterialApp(
-      home: NinjaCard(),
+void main() => runApp(const MaterialApp(
+      home: QuoteList(),
       debugShowCheckedModeBanner: false,
     ));
 
-class NinjaCard extends StatefulWidget {
+class QuoteList extends StatefulWidget {
+  const QuoteList({super.key});
+
   @override
-  State<NinjaCard> createState() => _NinjaCardState();
+  State<QuoteList> createState() => _QuoteListState();
 }
 
-class _NinjaCardState extends State<NinjaCard> {
-  int ninjaLevel = 0;
+class _QuoteListState extends State<QuoteList> {
+  List<Quote> quotes = [
+    Quote(text: "Be yourself", author: "oscar"),
+    Quote(text: "Be yourself", author: "oscar"),
+    Quote(text: "Be yourself", author: "oscar"),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.grey[900],
+      backgroundColor: Colors.grey,
       appBar: AppBar(
-        title: const Text("Ninja ID card"),
+        title: const Text("Awesome Quotes"),
         centerTitle: true,
-        backgroundColor: Colors.grey[850],
-        foregroundColor: Colors.white,
-        elevation: 0.0,
+        backgroundColor: Colors.redAccent,
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          setState(() {
-            ninjaLevel++;
-          });
-        },
-        child: Icon(Icons.add),
-        backgroundColor: Colors.grey,
-      ),
-      body: Padding(
-        padding: const EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            const Center(
-              child: CircleAvatar(
-                backgroundImage: AssetImage('assets/thumb.jpg'),
-                radius: 40.0,
-              ),
-            ),
-            const Divider(
-              height: 60.0,
-              color: Colors.grey,
-            ),
-            const Text(
-              'Name',
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 20.0,
-                letterSpacing: 2.0,
-              ),
-            ),
-            const SizedBox(height: 10.0),
-            const Text(
-              'Ninja Name',
-              style: TextStyle(
-                color: Colors.amberAccent,
-                fontSize: 35.0,
-                fontWeight: FontWeight.bold,
-                letterSpacing: 2.0,
-              ),
-            ),
-            const SizedBox(height: 35.0),
-            const Text(
-              'Current Ninja Level',
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 20.0,
-                letterSpacing: 2.0,
-              ),
-            ),
-            const SizedBox(height: 10.0),
-            Text(
-              '$ninjaLevel',
-              style: const TextStyle(
-                color: Colors.amberAccent,
-                fontSize: 20.0,
-                fontWeight: FontWeight.bold,
-                letterSpacing: 2.0,
-              ),
-            ),
-            const SizedBox(height: 30.0),
-            const Row(
-              children: [
-                Icon(Icons.email, color: Colors.grey),
-                SizedBox(width: 7),
-                Text(
-                  "hattori@ninja.com",
-                  style: TextStyle(
-                    color: Colors.grey,
-                    fontSize: 25.0,
-                    letterSpacing: 1.0,
-                  ),
-                ),
-              ],
-            ),
-          ],
-        ),
+      body: Column(
+        children: quotes
+            .map((quote) => Text('${quote.text} - ${quote.author}'))
+            .toList(),
       ),
     );
   }
