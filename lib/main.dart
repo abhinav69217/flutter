bd1a34fde938a78f951c34f191a6edd2b2698901
Abhinav <abhinav0045.be21@chitkara.edu.in>
2024-07-18 09:53:42 +0530

    stateful widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,54 +1,106 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(const MaterialApp(home: Home()));
+void main() => runApp(MaterialApp(
+      home: NinjaCard(),
+      debugShowCheckedModeBanner: false,
+    ));
 
-class Home extends StatelessWidget {
-  const Home({super.key});
+class NinjaCard extends StatefulWidget {
+  @override
+  State<NinjaCard> createState() => _NinjaCardState();
+}
 
+class _NinjaCardState extends State<NinjaCard> {
+  int ninjaLevel = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey[900],
       appBar: AppBar(
-        title: const Text("MY FIRST APP"),
-        backgroundColor: Colors.green[700],
+        title: const Text("Ninja ID card"),
         centerTitle: true,
+        backgroundColor: Colors.grey[850],
+        foregroundColor: Colors.white,
+        elevation: 0.0,
       ),
-      body: Row(
-        children: [
-          Expanded(child: Image.asset("assets/space-3.jpg"), flex: 3),
-          Expanded(
-            flex: 1,
-            child: Container(
-              padding: const EdgeInsets.all(30.0),
-              color: Colors.red,
-              child: const Text("1"),
-            ),
-          ),
-          Expanded(
-            flex: 1,
-            child: Container(
-              padding: const EdgeInsets.all(30.0),
-              color: Colors.pinkAccent,
-              child: const Text("2"),
-            ),
-          ),
-          Expanded(
-            flex: 1,
-            child: Container(
-              padding: const EdgeInsets.all(30.0),
-              color: Colors.amber,
-              child: const Text("3"),
-            ),
-          ),
-        ],
-      ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
       floatingActionButton: FloatingActionButton(
         onPressed: () {
-          print("Hello");
+          setState(() {
+            ninjaLevel++;
+          });
         },
-        backgroundColor: Colors.green[700],
-        child: const Icon(Icons.add),
+        child: Icon(Icons.add),
+        backgroundColor: Colors.grey,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Center(
+              child: CircleAvatar(
+                backgroundImage: AssetImage('assets/thumb.jpg'),
+                radius: 40.0,
+              ),
+            ),
+            const Divider(
+              height: 60.0,
+              color: Colors.grey,
+            ),
+            const Text(
+              'Name',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 20.0,
+                letterSpacing: 2.0,
+              ),
+            ),
+            const SizedBox(height: 10.0),
+            const Text(
+              'Ninja Name',
+              style: TextStyle(
+                color: Colors.amberAccent,
+                fontSize: 35.0,
+                fontWeight: FontWeight.bold,
+                letterSpacing: 2.0,
+              ),
+            ),
+            const SizedBox(height: 35.0),
+            const Text(
+              'Current Ninja Level',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 20.0,
+                letterSpacing: 2.0,
+              ),
+            ),
+            const SizedBox(height: 10.0),
+            Text(
+              '$ninjaLevel',
+              style: const TextStyle(
+                color: Colors.amberAccent,
+                fontSize: 20.0,
+                fontWeight: FontWeight.bold,
+                letterSpacing: 2.0,
+              ),
+            ),
+            const SizedBox(height: 30.0),
+            const Row(
+              children: [
+                Icon(Icons.email, color: Colors.grey),
+                SizedBox(width: 7),
+                Text(
+                  "hattori@ninja.com",
+                  style: TextStyle(
+                    color: Colors.grey,
+                    fontSize: 25.0,
+                    letterSpacing: 1.0,
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
